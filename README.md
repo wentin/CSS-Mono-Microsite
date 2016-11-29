5877bd82495ac214acd1a59817d15eb7c39a7721
wenzhang <zhangwenting111@gmail.com>
2016-11-29 06:40:39 +0100

    init commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# About CSS Mono Microsite
+
+ 
---
+++ js/app.js
@@ -0,0 +1,6 @@
+var cssMonoApp = angular.module('cssMonoApp', []);
+cssMonoApp.controller('ctrlSizing', function ($scope) {
+  $scope.fontSize = 6;
+  $scope.fontSizeArray = [6, 7, 8, 9, 10, 11, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 36, 40, 42, 44, 48, 54, 56, 60, 66, 72, 84, 88, 96, 100];
+});
+
